a7fd681256e92c6d60f0e42d74977e44e1c9ef2d
Nadim Moukhadder <nadim.moukhadder@orbyta.it>
2023-01-18 14:36:15 +0100

    update test yaml

--- src/test/java/com/fabrick/banking/feign/WireMockConfig.java
+++ src/test/java/com/fabrick/banking/feign/WireMockConfig.java
@@ -1,16 +1,18 @@
 package com.fabrick.banking.feign;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
 public class WireMockConfig {
 
-    private static final int PORT = 7070;
+    @Value("${feign.port}")
+    private int port;
     @Bean(initMethod = "start", destroyMethod = "stop")
     public WireMockServer mockClientService(){
-        return new WireMockServer(PORT);
+        return new WireMockServer(port);
     }
 
 }
